cadfd2ee4912f1f9a980a0fa17f6aa81a65e0d3e
max randle <maxrandle95@gmail.com>
2022-06-25 23:50:44 +1200

    test http fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { MDXProvider } from "@mdx-js/react";
 import MDXComponentMap from "@/components/MDXComponentMap";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MDXProvider components={MDXComponentMap}>
-      <Component {...pageProps} />
-    </MDXProvider>
+    <>
+      <Head>
+        <title>{`Max Randle`}</title>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
+      <MDXProvider components={MDXComponentMap}>
+        <Component {...pageProps} />
+      </MDXProvider>
+    </>
   );
 }
 
