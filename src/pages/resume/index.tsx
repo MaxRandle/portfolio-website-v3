3a62ced639e6770c479c4010c33502c1a4ca5191
max randle <maxrandle95@gmail.com>
2022-08-24 21:53:25 +1200

    more resume stuff

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -7,37 +7,68 @@ import { Heading } from "@/components/Heading";
 import { FiGithub, FiGlobe, FiMail, FiMapPin, FiPhone } from "react-icons/fi";
 import homePageContent from "@/content/homePage";
 
+const EXPERIENCE_START_DATE = new Date("2018-06-23");
+const MS_IN_YEAR = 3.154e10;
+const EXPERIENCE_IN_YEARS = Math.floor((new Date().valueOf() - EXPERIENCE_START_DATE.valueOf()) / MS_IN_YEAR);
+
+const CONTENT = {
+  name: "Max Randle",
+  contactDetails: {
+    email: "maxrandle95@gmail.com",
+    phone: "+64 21 023 069 08",
+    location: "Auckland, NZ",
+    website: "maxrandle.co.nz",
+    github: "github.com/MaxRandle",
+  },
+  personalStatement: [
+    `I am an intermediate-level software engineer with ${EXPERIENCE_IN_YEARS} years of professional experience delivering robust digital solutions using modern tools. I am primarily skilled at creating seamless user interfaces for functional web applications.`,
+    "Strong analytical and problem solving skills allow for tackling unusual and complex challenges. Forward thinking with the ability to anticipate blockers and communicate them early.",
+    "Exceptional written and oral communication skills with an ability to interface effectively across all management levels in an organisation. Personable, easy going, and comfortable engaging with clients to build and maintain trust.",
+    "A self motivated learner, driven to continually deepen and expand my skill set and committed to ongoing professional development. Able to quickly learn and adapt to new technologies and team cultures.",
+    "Exhibits effective time management and remains composed under pressure, identifying and communicating potential issues early.",
+    "Finger on the pulse of the web dev community and always on the lookout for new tools and best practices, as well as what is on the horizon for new web tech. Passionate about modern tools but has a realistic understanding of how and when to adopt them.",
+    "Enjoys collaborative work. Values and utilises the expertise team members. Actively participates in collective problem solving and experience sharing.",
+  ],
+};
+
 const Home: NextPage = () => {
-  function Contacts() {
+  function Hero() {
     return (
       <Box className="space-y-4 lg:space-y-8">
-        <Heading level={1}>Max Randle</Heading>
+        <Heading level={1}>{CONTENT.name}</Heading>
+      </Box>
+    );
+  }
+
+  function ContactDetails() {
+    return (
+      <Box className="space-y-4 lg:space-y-8">
         <ul className="space-y-4 lg:space-y-0 lg:space-x-4 lg:flex lg:justify-between">
           <li className="flex items-center">
             <FiMail size={24} />
             <Typography className="ml-6 lg:ml-4">
-              <a href="mailto:maxrandle95@gmail.com">maxrandle95@gmail.com</a>
+              <a href={`mailto:${CONTENT.contactDetails.email}`}>{CONTENT.contactDetails.email}</a>
             </Typography>
           </li>
           <li className="flex items-center">
             <FiPhone size={24} />
             <Typography className="ml-6 lg:ml-4">
-              <a href="tel:+64 21 023 069 08">+64 21 023 069 08</a>
+              <a href={`tel:${CONTENT.contactDetails.phone}`}>{CONTENT.contactDetails.phone}</a>
             </Typography>
           </li>
           <li className="flex items-center">
             <FiMapPin size={24} />
-            <Typography className="ml-6 lg:ml-4">Auckland, NZ</Typography>
+            <Typography className="ml-6 lg:ml-4">{CONTENT.contactDetails.location}</Typography>
           </li>
           <li className="flex items-center">
             <FiGlobe size={24} />
             <Typography className="ml-6 lg:ml-4">
               <a
-                href="https://maxrandle.co.nz"
+                href={`https://${CONTENT.contactDetails.website}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                maxrandle.co.nz
+                {CONTENT.contactDetails.website}
               </a>
             </Typography>
           </li>
@@ -45,11 +76,11 @@ const Home: NextPage = () => {
             <FiGithub size={24} />
             <Typography className="ml-6 lg:ml-4">
               <a
-                href="https://github.com/MaxRandle"
+                href={`https://${CONTENT.contactDetails.github}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                github.com/MaxRandle
+                {CONTENT.contactDetails.github}
               </a>
             </Typography>
           </li>
@@ -59,34 +90,10 @@ const Home: NextPage = () => {
   }
 
   function Intro() {
-    const EXPERIENCE_START_DATE = new Date("2018-06-23");
-    const MS_IN_YEAR = 3.154e10;
-    const EXPERIENCE_IN_YEARS = Math.floor((new Date().valueOf() - EXPERIENCE_START_DATE.valueOf()) / MS_IN_YEAR);
-
     return (
       <Box className="space-y-4 lg:space-y-8">
         <Heading level={2}>Personal statement</Heading>
-        <Typography>
-          Intermediate-level software developer with {EXPERIENCE_IN_YEARS} years of experience delivering robust digital solutions using modern tools.
-          <br />
-          <br />
-          Forward thinking with the ability to anticipate and preempt issues. Strong analytical and problem solving skills allow for tackling unusual
-          and complex challenges.
-          <br />
-          <br />
-          Exceptional written and oral communication skills with an ability to interface effectively across all management levels in an organisation.
-          Excellent at building and maintaining client relations, personable and comfortable engaging with clients to elicit requirements, present
-          solutions, and provide training.
-          <br />
-          <br />
-          A self motivated learner, driven to continually deepen and expand my skill set and committed to ongoing professional development. Able to
-          quickly learn and adapt to new technologies and environments. Exhibits effective time management and remains composed under pressure,
-          identifying and communicating potential issues early.
-          <br />
-          <br />
-          Enjoys collaborative work. Values and utilises the knowledge and abilities of my colleagues and team members. Actively participates in
-          collective problem solving and experience sharing.
-        </Typography>
+        <Typography>{CONTENT.personalStatement.join(" ")}</Typography>
       </Box>
     );
   }
@@ -138,11 +145,12 @@ const Home: NextPage = () => {
         dateRange: "March 2022 - present",
         companyName: "Kernel Wealth",
         responsibilities: [
-          "Writing unit tests and end to end tests.",
-          "Build accessible, context agnostic, functional react components that leverage the power of TypeScript with styled components and postcss plugins that facilitate next gen css features.",
-          "Setting new software patterns that are defined enough to scale but simple enough to be evolved with uncertain future requirements.",
-          "Working with NextJS to build functional web apps including form flows, as well as fast, static, and content rich web pages",
-          "Build pages that demonstrate dynamic, statically rendered content and are therefore blazing fast and do not contain stale data thanks to NextJS capability to do incremental static regeneration.",
+          "Built functional web apps With TypeScript/React/Next ",
+          "Built static marketing pages with dynamic content served from a CMS.",
+          "Setup end to end testing capabilities to run as a build step in bitbucket.",
+          "Precisely implement figma designs into clean, accessible, context agnostic, functional react components with multiple states and variants using Sass with styled-components and a BEM-inspired style system.",
+          "Quickly mocked up and prototyped pages and form flows using TailwindCSS to handle layout.",
+          "Set new software patterns that demonstrate flexibility, scalability, and maintainability",
         ],
       },
     ];
@@ -164,7 +172,8 @@ const Home: NextPage = () => {
     <PageLayout>
       <Section>
         <div className="container space-y-6 lg:space-y-12">
-          <Contacts />
+          <Hero />
+          <ContactDetails />
           <Intro />
           <Skills />
           <Experience />
